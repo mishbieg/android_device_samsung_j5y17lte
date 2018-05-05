6877c74bd0fc3c64c5f039e98e1df3d3b17c2b9e
ananjaser1211 <ananjaser@gmail.com>
2018-05-05 23:13:07 +0000

    #StayDirty

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,5 @@ add_lunch_combo rr_j5y17lte-userdebug
 add_lunch_combo rr_j5y17lte-eng
 add_lunch_combo aosp_j5y17lte-eng
 add_lunch_combo aosp_j5y17lte-userdebug
+add_lunch_combo du_j5y17lte-eng
+add_lunch_combo du_j5y17lte-userdebug
