5e2f66d73cdcf88e5daa5f33a9e7ac3d8f769d00
ananjaser1211 <ananjaser@gmail.com>
2018-05-09 03:51:22 +0400

    j5y17lte: Remove RR/LOS/AEX support

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,3 @@
-add_lunch_combo lineage_j5y17lte-userdebug
-add_lunch_combo lineage_j5y17lte-eng
-add_lunch_combo rr_j5y17lte-userdebug
-add_lunch_combo rr_j5y17lte-eng
 add_lunch_combo aosp_j5y17lte-eng
 add_lunch_combo aosp_j5y17lte-userdebug
 add_lunch_combo du_j5y17lte-eng
